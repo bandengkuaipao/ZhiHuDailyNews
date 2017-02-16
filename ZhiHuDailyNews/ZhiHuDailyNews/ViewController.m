836b67292a19ca558c7ad82ba2d9ba78ada099a2
bob <744865006@qq.com>
2017-02-16 11:07:30 +0800

    创建项目并完成Pods下载

---
+++ ZhiHuDailyNews/ZhiHuDailyNews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZhiHuDailyNews
+//
+//  Created by bob on 17/2/16.
+//  Copyright © 2017年 bandeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ZhiHuDailyNews/ZhiHuDailyNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZhiHuDailyNews
+//
+//  Created by bob on 17/2/16.
+//  Copyright © 2017年 bandeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
